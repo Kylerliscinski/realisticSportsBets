78a03d1c6da1fb0e3c440e9dbe9bd1cb87ee6b89
Kylerliscinski <62256404+Kylerliscinski@users.noreply.github.com>
2024-03-20 15:18:48 -0600

    Init

---
+++ app.js
@@ -0,0 +1,162 @@
+let bank = 100
+
+const players = [
+  {
+    name: "D'Marcus Williums",
+    teamNumber: 0,
+    emoji: '🏃‍♂️',
+    skill: 10
+  },
+  {
+    name: "Tyroil Smoochie-Wallace",
+    teamNumber: 0,
+    emoji: '🤾‍♂️',
+    skill: 30
+  },
+  {
+    name: "Jackmerius Tacktheratrix",
+    teamNumber: 0,
+    emoji: '🏇',
+    skill: 88
+  },
+  {
+    name: "Javaris Jamar Javarison-Lamar",
+    teamNumber: 0,
+    emoji: '🏌️‍♀️',
+    skill: 15
+  },
+  {
+    name: "D'Pez Poopsie",
+    teamNumber: 0,
+    emoji: '🏋️‍♂️',
+    skill: 77
+  },
+  {
+    name: "D'Jasper Probincrux III",
+    teamNumber: 0,
+    emoji: '🏌️‍♂️',
+    skill: 21
+  },
+  {
+    name: "Leoz Maxwell Jilliumz",
+    teamNumber: 0,
+    emoji: '🤾',
+    skill: 5
+  },
+  {
+    name: "Hingle McCringleberry",
+    teamNumber: 0,
+    emoji: '🏂',
+    skill: 99
+  },
+  {
+    name: "L'Carpetron Dookmarriot",
+    teamNumber: 0,
+    emoji: '🧘‍♀️',
+    skill: 50
+  },
+  {
+    name: "Xmus Jaxon Flaxon-Waxon",
+    teamNumber: 0,
+    emoji: '🚶‍♀️',
+    skill: 1
+  },
+  {
+    name: "Saggitariutt Jefferspin",
+    teamNumber: 0,
+    emoji: '🏋️‍♀️',
+    skill: 61
+  },
+  {
+    name: "Quatro Quatro",
+    teamNumber: 0,
+    emoji: '🤺',
+    skill: 34
+  },
+  {
+    name: "X-Wing @Aliciousness",
+    teamNumber: 0,
+    emoji: '🏄',
+    skill: 71
+  },
+  {
+    name: "Bisquiteen Trisket",
+    teamNumber: 0,
+    emoji: '🧜‍♂️',
+    skill: 76
+  },
+  {
+    name: "Scoish Velociraptor Maloish",
+    teamNumber: 0,
+    emoji: '🤸',
+    skill: 47
+  },
+  {
+    name: "Donkey Teeth",
+    teamNumber: 0,
+    emoji: '⛹️‍♀️',
+    skill: 23
+  },
+  {
+    name: "T.J. A.J. R.J. Backslashinfourth V",
+    teamNumber: 0,
+    emoji: '🕴️',
+    skill: 58
+  },
+  {
+    name: "Firstname Lastname",
+    teamNumber: 0,
+    emoji: '💃',
+    skill: 99
+  },
+  {
+    name: "Dan Smith",
+    teamNumber: 0,
+    emoji: '🧍‍♂️',
+    skill: 3
+  },
+  {
+    name: "Tiger",
+    teamNumber: 0,
+    emoji: '🐅',
+    skill: 100
+  },
+]
+
+draftPlayers()
+
+function draftPlayers(player) {
+  // console.log("Drafting Players")
+  let playerHTML = ''
+  players.forEach((player) => {
+    // console.log(player.name);
+    playerHTML += `<span title="${player.name}">${player.emoji}</span>`
+  })
+  // console.log('all players', playerHTML);
+  const playerDraftElm = document.getElementById('draft-players')
+  playerDraftElm.innerHTML = playerHTML
+}
+
+function assignTeamOne() {
+  let playerHTML = ''
+  players.forEach((player) => {
+    player.teamNumber = Math.round(Math.random() + 1 * 1)
+    if (player.teamNumber == 1) {
+      playerHTML += `<span title="${player.name}">${player.emoji}</span>`
+      const teamOne = document.getElementById('team-one')
+      teamOne.innerHTML = playerHTML
+    }
+  })
+}
+
+function assignTeamTwo() {
+  let playerHTML = ''
+  players.forEach((player) => {
+    player.teamNumber = Math.round(Math.random() + 1 * 1)
+    if (player.teamNumber == 2) {
+      playerHTML += `<span title="${player.name}">${player.emoji}</span>`
+      const teamOne = document.getElementById('team-two')
+      teamOne.innerHTML = playerHTML
+    }
+  })
+}
